64f0f66d4222b961e4853101dd5b4c292ca32915
Jaypee <jpisanu@gmail.com>
2024-08-27 23:40:43 -0300

    logged in out

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
-
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
+  const user = useUser();
+  const session = user.user?.sub ? true : false;
   return (
     <div>
-      <a href="/api/auth/login">Login</a>
-      <br/>
-      <a href="/api/auth//logout">Logout</a>
-      <br/>
+      {!session ? (
+        <a href="/api/auth/login">Login</a>
+      ) : (
+        <a href="/api/auth//logout">Logout</a>
+      )}
+      <br />
       <a href="/profile">Profile</a>
-      <br/>
+      <br />
       <a href="/lines">Lines</a>
     </div>
   );
